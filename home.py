783b5b49a28a198016f27a527050e0862028d1a1
Jonty Morris <jonty.morris@yahoo.co.nz>
2019-10-25 12:29:35 +1300

    Created home menu context

---
+++ home.py
@@ -0,0 +1,22 @@
+"""
+Contains the class for the game's home screen
+"""
+
+
+class HomeContext:
+    """
+    Used for the game's home screen
+    """
+
+    def __init__(self):
+        pass
+
+    def update(self):
+        """
+        Updates the home screen
+        """
+
+    def render(self):
+        """
+        Draws the home screen
+        """
