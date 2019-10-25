d89ab466b282a6adf1c4f581cb3bbc3e14a25b76
Jonty Morris <jonty.morris@yahoo.co.nz>
2019-10-25 21:17:11 +1300

    Shortened long lines

--- home.py
+++ home.py
@@ -14,9 +14,12 @@ class HomeContext:
     def __init__(self, surface):
         self.surface = surface
 
-        self.desert_image = pyui.Image(surface, assets.DESERT_IMAGE, (0, 0), (1920, 1080))
-        self.cannon_image = pyui.Image(surface, assets.CANNON_IMAGE, (520, 200), (880, 468))
-        self.play_button = pyui.Button(surface, 'Play Cannons', (710, 750))
+        self.desert_image = pyui.Image(
+            surface, assets.DESERT_IMAGE, (0, 0), (1920, 1080))
+        self.cannon_image = pyui.Image(
+            surface, assets.CANNON_IMAGE, (520, 200), (880, 468))
+        self.play_button = pyui.Button(
+            surface, 'Play Cannons', (710, 750))
 
     def update(self):
         """
