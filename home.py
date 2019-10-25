f0645ddfd47ea4da8143012014df807fb17acbd5
Jonty Morris <jonty.morris@yahoo.co.nz>
2019-10-25 21:16:18 +1300

    Added elements to screen

--- home.py
+++ home.py
@@ -13,12 +13,11 @@ class HomeContext:
     """
 
     def __init__(self, surface):
-        self.test_label = pyui.Label(
-            surface,
-            'Hello there',
-            (0, 100),
-            (assets.ABOVE_FONT, 12, colors.BLUE)
-        )
+        self.surface = surface
+
+        self.desert_image = pyui.Image(surface, assets.DESERT_IMAGE, (0, 0), (1920, 1080))
+        self.cannon_image = pyui.Image(surface, assets.CANNON_IMAGE, (520, 200), (880, 468))
+        self.play_button = pyui.Button(surface, 'Play Cannons', (710, 750))
 
     def update(self):
         """
@@ -30,4 +29,6 @@ class HomeContext:
         Draws the home screen
         """
 
-        self.test_label.render()
+        self.desert_image.render()
+        self.cannon_image.render()
+        self.play_button.render()
