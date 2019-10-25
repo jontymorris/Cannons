24088e9cce6e45e8a8c95c38caa1608eda767d3c
Jonty Morris <jonty.morris@yahoo.co.nz>
2019-10-25 21:13:07 +1300

    Added new assets

--- assets.py
+++ assets.py
@@ -1,9 +1,15 @@
 """
-File containing all the asset paths
+Contains all asset paths
 """
 
 from os.path import join
 
 
 BASE = join('.', 'assets')
-ABOVE_FONT = join(BASE, 'fonts', 'above.ttf')
+
+ABOVE_FONT = join(BASE, 'above.ttf')
+
+CANNON_IMAGE = join(BASE, 'cannon.png')
+DESERT_IMAGE = join(BASE, 'desert.png')
+BUTTON_NORMAL = join(BASE, 'button_normal.png')
+BUTTON_ACTIVE = join(BASE, 'button_active.png')
