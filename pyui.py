e311369ccc3e35a4eba7a4119780b05e5464cea4
Jonty Morris <jonty.morris@yahoo.co.nz>
2019-10-25 19:35:58 +1300

    Removed height center

--- pyui.py
+++ pyui.py
@@ -1,24 +1,24 @@
 """
-Custom objects for making an interface
+Make user interfaces with ease
 """
 
 import pygame
 
 
 class Label:
-    '''
+    """
     Object for displaying text
-    '''
+    """
 
     def __init__(self, surface, text, position, font_info):
         self.surface = surface
 
         font = pygame.font.Font(font_info[0], font_info[1])
-
         self.label = font.render(text, True, font_info[2])
+
         self.rect = self.label.get_rect()
-        self.rect.center = (position[0], position[1])
-        self.rect = self.rect.move(self.rect.width/2, 0)
+        self.rect.center = position
+        self.rect = self.rect.move(self.rect.width/2, self.rect.height/2)
 
     def render(self):
         """
